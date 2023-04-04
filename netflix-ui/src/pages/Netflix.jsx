92a98aaf5e25cc2a4ca1a932456132f1682bbceb
hoysal08 <suraj_yahoo.com>
2023-04-04 23:09:07 +0530

    authentication setup done

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavBar from '../components/NavBar';
+import backgroundImage from '../assets/home.jpg';
+import MovieLogo from "../assets/homeTitle.webp";
+import { FaPlay } from 'react-icons/fa';
+import {AiOutlineInfoCircle} from 'react-icons/ai';
 
 function Netflix() {
+
+  const [isScrolled,setIsScrolled]=useState(false);
+
+  window.onscroll=()=>{
+    setIsScrolled(window.pageYOffset === 0 ? false: true);
+    return ()=>(window.onscroll=null);
+  }
   return (
-    <div>Netflix</div>
+    <div>
+    <NavBar isScrolled={isScrolled}/>
+    <div className="hero">
+      <img src={backgroundImage} alt="background" className='background-image'/>
+      <div className="container">
+        <div className="logo">
+          <img src={MovieLogo} alt="MovieLogo"/>
+        </div>
+        <div className="buttons flex">
+          <button className='flex j-center a-center'>
+            <FaPlay/>Play
+          </button>
+          <button className="flex j-center a-center">
+            <AiOutlineInfoCircle/>More Info
+          </button>
+        </div>
+      </div>
+    </div>
+
+    </div>
   )
 }
 
